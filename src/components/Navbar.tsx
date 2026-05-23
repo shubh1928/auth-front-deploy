3e3d013220b40b9e75ec0b0296299f51f92f20a4
Shubham <shubhamgopale16@gmail.com>
2026-05-23 23:38:14 +0530

    added dark light toggle button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Button } from './ui/button'
 import { NavLink, useNavigate } from 'react-router'
 import useAuth from '@/auth/store';
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
 
 function Navbar() {
 
@@ -9,8 +11,40 @@ const user = useAuth((state) => state.user);
 const logout = useAuth((state) => state.logout);
 const navigate = useNavigate();
 
+const [darkMode, setDarkMode] = useState(
+  localStorage.getItem("theme") !== "light"
+);
+
+useEffect(() => {
+
+  const storedTheme = localStorage.getItem("theme");
+
+  if (storedTheme === "light") {
+    document.documentElement.classList.remove("dark");
+    setDarkMode(false);
+  } else {
+    document.documentElement.classList.add("dark");
+    setDarkMode(true);
+  }
+
+}, []);
+
+
+const toggleTheme = () => {
+
+  if (darkMode) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  }
+
+  setDarkMode(!darkMode);
+};
+
   return (
-    <nav className='py-5 dark:border-b border-gray-600 md:py-0 flex md:flex-row gap-4 md:gap-0 flex-col md:h-14 items-center justify-around p-4 dark:text-white'>
+    <nav className='py-5 dark:border-b border-gray-600 sm:py-0 flex sm:flex-row gap-4 sm:gap-0 flex-col sm:h-14 items-center justify-around p-4 dark:text-white'>
         {/* brand */}
         <div className='font-semibold items-center flex gap-2'>
             <span className='inline-block text-center h-6 w-6 rounded-md bg-gradient-to-r from-primary to-primary/40'>{"A"}</span>
@@ -19,7 +53,20 @@ const navigate = useNavigate();
 
         <div className='flex  gap-4 items-center'> 
 
+                                        <Button
+                                        onClick={toggleTheme}
+                                        variant="outline"
+                                        className="rounded-xl"
+                                        >
+                                        {darkMode ? (
+                                            <Sun className="h-5 w-5" />
+                                        ) : (
+                                            <Moon className="h-5 w-5" />
+                                        )}
+                                        </Button>
+
             {
+                
                 checkLogin() ? 
                 (<>
                             <NavLink to={"/dashboard/profile"}>{user?.name}</NavLink>
@@ -29,7 +76,7 @@ const navigate = useNavigate();
                 : 
                 (<>
                             <NavLink to={"/"} className={({isActive}) => isActive ? "text-primary" : ""}>
-                                <a href="#">Home</a>
+                                Home
                             </NavLink>
                             <NavLink to={"/login"} className={({isActive}) => isActive ? "text-primary" : ""}>
                                 <Button size={"sm"} className='cursor-pointer'>Login</Button>      
