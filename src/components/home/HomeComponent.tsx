37d14fd6b0d871f2f7d9f20254f4a5b57d018cdb
Shubham <shubhamgopale16@gmail.com>
2026-05-23 20:53:19 +0530

    added footer

--- src/components/home/HomeComponent.tsx
+++ src/components/home/HomeComponent.tsx
@@ -337,21 +337,7 @@ function HomeComponent() {
         </Card>
       </section>
 
-      {/* Footer */}
-      <footer className="relative z-10 border-t border-black/10 dark:border-white/10 bg-white/40 dark:bg-black/30 backdrop-blur-2xl">
-        <div className="container mx-auto px-6 py-8 flex flex-col md:flex-row items-center justify-between gap-4">
-          <h2 className="text-2xl font-black tracking-wider">
-            AUTH<span className="text-cyan-400">APP</span>
-          </h2>
 
-          <div className="flex gap-6 text-sm text-gray-600 dark:text-gray-400">
-            <a href="#">Privacy</a>
-            <a href="#">Security</a>
-            <a href="#">Terms</a>
-            <a href="#">Support</a>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router'
 import Navbar from '../components/Navbar'
 import { Toaster } from 'react-hot-toast'
+import Footer from '@/components/Footer'
 
 function RootLayout() {
   return (
@@ -8,6 +9,7 @@ function RootLayout() {
         <Toaster />
         <Navbar />
         <Outlet />
+        <Footer />
     </div>
   )
 }
